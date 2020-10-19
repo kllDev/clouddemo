1dac69bc17604475cfe4b650d57e529f6ade45d9
keliangliang <kll_112588@163.com>
2020-10-19 10:19:43 +0800

    test

---
+++ consumer/src/main/java/com/alibaba/nacos/example/spring/cloud/tes11.java
@@ -0,0 +1,11 @@
+package com.alibaba.nacos.example.spring.cloud;
+
+/**
+ * @author liangliang.ke
+ * @version 1.0
+ * @program clouddemo
+ * @description 111
+ * @date 2020-10-19 10:19
+ */
+public class tes11 {
+}
